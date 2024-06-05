d77f010a66f8cbe57ec8937f2e9d1b50179162fa
sakib6619 <sahadothosain6619@gmail.com>
2024-06-05 16:15:42 +0600

    coustom user setup and admin setup

--- SETTINGS/urls.py
+++ SETTINGS/urls.py
@@ -1,10 +1,12 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('account/', include('app_login.urls')),
+    path('', include('app_shop.urls')),
 ]
 
 if settings.DEBUG:
--- app_shop/apps.py
+++ app_shop/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class AppConfig(AppConfig):
+class AppShopConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'APP'
+    name = 'app_shop'
