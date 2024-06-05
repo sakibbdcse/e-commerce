a5e55624bc21b0b9dc337002e4aed78e2ebd2b77
sakib6619 <sahadothosain6619@gmail.com>
2024-06-05 17:47:50 +0600

    coustom user create succesfull

--- app_login/models.py
+++ app_login/models.py
@@ -1,23 +1,19 @@
 from django.db import models
-# to create a custom user models and admin penel
-
-from django.contrib.auth.models import BaseUserManager, AbstractBaseUser, PermissionsMixin
-# from django.utils.translation import ugettext_lazy
-
-# Create your models here.
-
-class myUserManager (BaseUserManager):
+from django.contrib.auth.models import BaseUserManager, AbstractBaseUser, PermissionsMixin, Group, Permission
+from django.utils.translation import gettext_lazy as _
+# when user create craete user realated profile and linked
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+class MyUserManager(BaseUserManager):
 
     def _create_user(self, email, password, **extra_fields):
         if not email:
-            raise ValueError('email reqired!')
+            raise ValueError('Email is required!')
         email = self.normalize_email(email)
         user = self.model(email=email, **extra_fields)
         user.set_password(password)
         user.save(using=self._db)
         return user
-    
-    # supper user creation
 
     def create_superuser(self, email, password, **extra_fields):
         extra_fields.setdefault('is_staff', True)
@@ -25,8 +21,78 @@ class myUserManager (BaseUserManager):
         extra_fields.setdefault('is_active', True)
 
         if extra_fields.get('is_staff') is not True:
-            raise ValueError('super user must have is_staff = Ture')
+            raise ValueError('Superuser must have is_staff=True')
         if extra_fields.get('is_superuser') is not True:
-            raise ValueError('super user must be is_superuser=True')
+            raise ValueError('Superuser must have is_superuser=True')
         
         return self._create_user(email, password, **extra_fields)
+
+class User(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(unique=True, null=False, blank=False)
+    is_staff = models.BooleanField(
+        _('staff status'),
+        default=False,
+        help_text= _('designaes whether the user can log in this site')
+    )
+    is_active = models.BooleanField(
+        _('active'),
+        default=True
+    )
+    date_joined = models.DateTimeField(_('date joined'), auto_now_add=True)
+
+    # Add related_name to avoid clashes
+    groups = models.ManyToManyField(
+        Group,
+        related_name='custom_user_set',
+        blank=True,
+        help_text=_('The groups this user belongs to. A user will get all permissions granted to each of their groups.'),
+        verbose_name=_('groups')
+    )
+    user_permissions = models.ManyToManyField(
+        Permission,
+        related_name='custom_user_set',
+        blank=True,
+        help_text=_('Specific permissions for this user.'),
+        verbose_name=_('user permissions')
+    )
+
+    objects = MyUserManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    def __str__(self):
+        return self.email
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
+    username = models.CharField(max_length=264, blank=True)
+    full_name = models.CharField(max_length=264, blank=True)
+    address_1 = models.TextField(max_length=300, blank=True)
+    city = models.CharField(max_length=50, blank=True)
+    zipcode = models.CharField(max_length=10, blank=True)
+    country = models.CharField(max_length=50, blank=True)
+    phone = models.CharField(max_length=20, blank=True)
+    prof_pic= models.ImageField(upload_to='prof_def/', default='prof_def/defProf.jpg')
+    date_joined = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.user + "'s profile"
+    
+    def is_fully_fieled(self):
+        fields_names = [f.name for f in self._meta.get_fields()]
+
+        for field_name in fields_names:
+            value = getattr(self, field_name)
+            if value is None or value=='':
+                return False
+        return True
+    
+@receiver(post_save, sender=User)
+def create_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+
+@receiver(post_save, sender=User)
+def save_profile(sender, instance, **kwargs):
+    instance.profile.save()
