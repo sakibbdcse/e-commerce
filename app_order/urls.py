556ed62580232231d7602a2640327a52bf3f60c2
sakib6619 <sahadothosain6619@gmail.com>
2024-06-06 21:51:44 +0600

    added card product successfull

--- app_order/urls.py
+++ app_order/urls.py
@@ -1,5 +1,10 @@
 from django.urls import path
+from app_shop import views as shop_views
+from app_order import views as order_views
 
 urlpatterns = [
-    # path('', views.Home.as_view(), name='home'),
+    path('', shop_views.Home.as_view(), name='home'),
+    path('product/<pk>/', shop_views.ProductDetails.as_view(), name='product-details'),
+    path('add-to-cart/<pk>/', order_views.add_to_cart, name='add_to_cart'),
+    path('cart/', order_views.cart_view, name='cart'),
 ]
