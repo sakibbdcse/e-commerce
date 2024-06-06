51dab107206ea0d95af160cf75e127b6d1dee71d
sakib6619 <sahadothosain6619@gmail.com>
2024-06-06 22:40:44 +0600

    added process to checkout payment

--- app_order/models.py
+++ app_order/models.py
@@ -2,7 +2,6 @@ from django.db import models
 from django.conf import settings
 from app_shop.models import Product
 
-# Create your models here.
 class Cart(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='cart')
     item = models.ForeignKey(Product, on_delete=models.CASCADE)
@@ -16,7 +15,8 @@ class Cart(models.Model):
     
     def get_total(self):
         total = self.item.price * self.quantity
-        return format(total, '0.2f')
+        total_float = format(total, '0.2f')
+        return total_float
 
 class Order(models.Model):
     orderitems = models.ManyToManyField(Cart)
@@ -26,11 +26,8 @@ class Order(models.Model):
     paymentID = models.CharField(max_length=264, blank=True, null=True)
     orderId = models.CharField(max_length=200, blank=True, null=True)
 
-    def __str__(self):
-        return f'Order {self.id} by {self.user}'
-    
     def get_totals(self):
         total = 0
-        for order_item in self.orderitems.all():
-            total += float(order_item.get_total())
-        return format(total, '0.2f')
+        for order_items in self.orderitems.all():
+            total += float(order_items.get_total())
+        return total
