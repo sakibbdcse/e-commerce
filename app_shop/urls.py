25391bb8048bf695315711a8b99c4b6fa94ffc12
sakib6619 <sahadothosain6619@gmail.com>
2024-06-06 18:10:04 +0600

    start working order sections

--- app_shop/urls.py
+++ app_shop/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
 from app_shop import views
+
 urlpatterns = [
     path('', views.Home.as_view(), name='home'),
+    path('product/<int:pk>/', views.ProductDetails.as_view(), name='product-details'),
+    path('add-to-cart/<int:product_id>/', views.add_to_cart, name='add-to-cart'),
 ]
--- app_shop/views.py
+++ app_shop/views.py
@@ -1,9 +1,31 @@
-from django.shortcuts import render
-from django.views.generic import ListView
+from django.shortcuts import render, redirect
+from django.views.generic import ListView, DetailView
 from app_shop.models import Product
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 # Create your views here.
 class Home(ListView):
     model = Product
     template_name = 'app_shop/home.html'
     context_object_name = 'products'
+
+class ProductDetails(DetailView):
+    model = Product
+    template_name = 'app_shop/product_details.html'
+
+def add_to_cart(request, product_id):
+    product = Product.objects.get(id=product_id)
+    cart = request.session.get('cart', {})
+    
+    if product_id in cart:
+        cart[product_id]['quantity'] += 1
+    else:
+        cart[product_id] = {
+            'name': product.name,
+            'price': product.price,
+            'quantity': 1,
+            'image': product.mainimage.url,
+        }
+    
+    request.session['cart'] = cart
+    return redirect('home')
